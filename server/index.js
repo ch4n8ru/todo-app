378b98f41620d20d137e5c7a81e40d21fb19615b
chandra <prakashchandra.cp@outlook.com>
2020-12-22 21:54:29 +0530

    added test case for auth checker

--- server/index.js
+++ server/index.js
@@ -16,9 +16,9 @@ app.use(cookieParser());
 
 app.use('/auth', AuthRoutes);
 
-// app.use(authChecker, (req, res, next) => {
-//     res.json(req.body.user)
-// })
+app.use(authChecker, (req, res, next) => {
+    res.json(req.body.user)
+})
 
 app.use(handleErrors);
 
@@ -26,7 +26,7 @@ try {
     mongoose.connect(process.env.MONGO_URI, { dbName: process.env.MONGO_DB_NAME, useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 }
 catch (err) {
-    console.log("Couldnt connet to database " + err )
+    console.log("Couldnt connect to database " + err )
 }
 
 mongoose.connection.on('open', () => {
