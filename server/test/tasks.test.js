71408a55e6183cfa6ab2be9bb390348bf9dace53
chandra <prakashchandra.cp@outlook.com>
2020-12-24 21:37:53 +0530

    add test for getall without since parameter

--- server/test/tasks.test.js
+++ server/test/tasks.test.js
@@ -7,16 +7,33 @@ chai.use(chaiHttp);
 const allTasks = require("./testdata/tasks.json")
 const resultForSince = require("./testdata/allwithsince.json")
 
+
+// 5fe378ad64f0ae2fba24a3bc
+
+
 describe("tasks tests", () => {
     describe("GET /tasks/all", () => {
         it("should return last 10 days worth of tasks when since parameter is passed", done => {
             chai
             .request(app)
             .get('/task/all?since=1606780800')
-            .end((res, end) => {
+            .end((res, err) => {
                 expect(res.status).to.equal(200, "should return 200 status");
-                expect(res.body).length.to.equals(15, "should return 15 entries");
+                expect(res.body).length.to.equals(5, "should return 5 entries");
                 expect(res.body).to.eql(resultForSince)
+                if(err) done(err);
+                done();
+            })
+        })
+
+        it("should return the latest 10 entries when since parameter is not provided", done => {
+            chai
+            .request(app)
+            .get('/task/all')
+            .end((res,err) => {
+                expect(res.status).to.equal(200, "should return 200 status");
+                expect(res.body).length.to.equals(10, "should return 10 entries");
+                expect(res.body).to.eql(allTasks)
                 if(err) done(err);
                 done();
             })
